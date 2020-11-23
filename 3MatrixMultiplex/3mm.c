2f34fc5cb648328b889c236c13d1d09bc28b2cae
Ubsefor <letzdropthebas@gmail.com>
2020-11-24 02:06:09 +0300

    Fix2

--- 3MatrixMultiplex/3mm.c
+++ 3MatrixMultiplex/3mm.c
@@ -28,7 +28,7 @@ void bench_timer_stop( void )
 
 void bench_timer_print( void )
 {
-#ifdef BENCH
+#ifndef BENCH
     printf( "Time in seconds = %0.6lf\n", bench_t_end - bench_t_start );
 #else
     printf( "%0.6lf\n", bench_t_end - bench_t_start );
